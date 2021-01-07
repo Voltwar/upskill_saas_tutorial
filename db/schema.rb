60377dd8703a62396191eef9c6373bb12b9c45f5
Nick Zaker <nczaker@gmail.com>
2021-01-07 13:59:46 +0000

    added hirb gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20210107134009) do
 
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
