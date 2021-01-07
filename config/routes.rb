bf3f86999cf8c8c29f337bf52028ed32005a630e
Nick Zaker <nczaker@gmail.com>
2021-01-07 13:44:57 +0000

    Added create action to save to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :books
+  resources :users
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     resources :contacts
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210107122038) do
+ActiveRecord::Schema.define(version: 20210107134009) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
